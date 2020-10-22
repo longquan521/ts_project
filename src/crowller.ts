b243303ae7b100720dd3f4b1df7d5ce65ba654a2
longquan521 <569978412@qq.com>
2020-10-22 21:02:05 +0800

    新增插件，去获取页面

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,1 +1,17 @@
-console.log('Hello Word');
+// cnpm i superagent --save爬虫依赖工具
+// ts直接引用js会报错，ts->.d.ts 翻译文件->js
+import superagent from 'superagent';
+class Crowller {
+  private secret = 'secretKey';
+  url = 'https://longquan521.github.io/index.html';
+  private rawHtml = '';
+  async getRawHtml() {
+    const result = await superagent.get(this.url);
+    console.log(result.text);
+    this.rawHtml = result.text;
+  }
+  constructor() {
+    this.getRawHtml();
+  }
+}
+const crowller = new Crowller();
